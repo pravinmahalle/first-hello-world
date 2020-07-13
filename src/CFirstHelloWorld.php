c535056937b43ed245a7c2b1855cbbec56451df5
pravinmahalle <pravinmahalle@outlook.com>
2020-07-13 13:59:25 +0530

    First message

---
+++ src/CFirstHelloWorld.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace firstHello\CFirstHelloWorld;
+
+class CFirstHelloWorld {
+
+	public function __construct() {
+		// constructor body
+	}
+
+	public function printMessage(){
+		return "Hello World";
+	}
+}
